34df7e89eb8073407c7eafe5caba66b162196ffb
Anupong lewtoksan <comnet58.33621@gmail.com>
2024-07-31 16:40:16 +0700

    update htaccess

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // components
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer";
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import FAQ from './pages/FAQ'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import FAQ from "./pages/FAQ";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-        <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        <Footer />
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
